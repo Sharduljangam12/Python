09875cc5eaeef8299a98170f41ef9a0f9481c5bc
Sharduljangam12 <152073842+Sharduljangam12@users.noreply.github.com>
2025-07-09 23:28:21 +0530

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,7 @@
+#Find that the given number is even or odd
+
+x = int(input("Enter the number you want to Check is Even or Odd:-  "))
+if x%2 == 0: 
+    print("The Number you entered is Even")
+else:
+    print("The Number you entered is Odd")
